a1b88b4ba874fb621d52cbecf55a138340dd2362
Chkh0ng's MacBook Pro <chkh0ng@Chkh0ngsui-MacBook-Pro.local>
2018-10-15 16:03:39 +0900

    Initial commit

---
+++ AppCodeTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppCodeTest
+//
+//  Created by Chkh0ng's MacBook Pro on 05/10/2018.
+//  Copyright © 2018 Chkh0ng's MacBook Pro. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+override func viewDidLoad() {
+    super.viewDidLoad()
+    print("AppCodeTest!!")
+    print("AppCodeTest22!!")
+}
+
+}
